03b94e44f00f361dab072fedc5399856fe4c86cc
billythedummy <dhy1996@live.com.sg>
2024-01-19 11:45:59 +0800

    add remaining LSTs

--- sanctum-lst-list/tests/main.rs
+++ sanctum-lst-list/tests/main.rs
@@ -1,10 +1,19 @@
-use sanctum_lst_list::SanctumLstList;
+use std::collections::HashSet;
+
+use sanctum_lst_list::{SanctumLst, SanctumLstList};
 
 #[test]
 fn load() {
     // make sure this doesnt panic
-    let _list = SanctumLstList::load();
-    // TODO: sample 1 from each enum variant to make sure everything works
-    // as expected
-    //println!("{_list:?}");
+    let SanctumLstList { sanctum_lst_list } = SanctumLstList::load();
+
+    // validate unique mints
+    let mut dedup = HashSet::new();
+    for SanctumLst { mint, .. } in sanctum_lst_list {
+        if !dedup.insert(mint) {
+            panic!("Duplicate mint {mint} found");
+        }
+    }
+    // TODO: sample 1 from each enum variant to check the fields values
+    // to make sure everything is deserialized as expected
 }

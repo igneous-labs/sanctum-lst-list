9d8cb221b9d57add727469e6d71a330ad09d124d
billythedummy <dhy1996@live.com.sg>
2024-07-30 17:49:57 +0800

    check logo uri too

--- rust/sanctum-lst-list/tests/tests/metadata.rs
+++ rust/sanctum-lst-list/tests/tests/metadata.rs
@@ -2,14 +2,16 @@ use std::{collections::HashSet, error::Error};
 
 use mpl_token_metadata::accounts::Metadata;
 use sanctum_lst_list::{SanctumLst, SanctumLstList};
-use solana_client::rpc_client::RpcClient;
+use serde::Deserialize;
+use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::commitment_config::CommitmentConfig;
 
 use crate::common::SOLANA_RPC_URL;
 
-#[test]
-fn verify_all_lsts_token_metadata() {
-    let rpc = RpcClient::new(SOLANA_RPC_URL);
+#[tokio::test]
+async fn verify_all_lsts_token_metadata() {
+    let client: &'static reqwest::Client = Box::leak(Box::new(reqwest::Client::new()));
+    let rpc = RpcClient::new(SOLANA_RPC_URL.to_owned());
     let SanctumLstList { sanctum_lst_list } = SanctumLstList::load();
     // just do it sequentially to avoid rpc limits
     let mut no_metadata_lsts = Vec::new();
@@ -31,12 +33,14 @@ fn verify_all_lsts_token_metadata() {
         "stepSOL", // name is `Step Staked SOL` but onchain is `stepSOL`
         "SOL",     // name is `SOL` but onchain is `Wrapped SOL`
         "mSOL",    // name is `Marinade staked SOL` but onchain is `Marinade staked SOL (mSOL)`
+        "spikySOL", // malformed linked metadata json
+        "apySOL",  // missing metadata json
     ]
     .into_iter()
     .collect();
 
     for sanctum_lst in sanctum_lst_list.iter() {
-        match verify_lst_token_metadata(&rpc, sanctum_lst, &ignore_symbols) {
+        match verify_lst_token_metadata(client, &rpc, sanctum_lst, &ignore_symbols).await {
             Ok(Some(..)) => (),
             Ok(None) => {
                 no_metadata_lsts.push(&sanctum_lst.symbol);
@@ -51,10 +55,20 @@ fn verify_all_lsts_token_metadata() {
     }
 }
 
-fn verify_lst_token_metadata(
+#[derive(Debug, Deserialize)]
+struct OffchainMetadata {
+    image: String,
+}
+
+async fn verify_lst_token_metadata(
+    client: &reqwest::Client,
     rpc: &RpcClient,
     SanctumLst {
-        mint, symbol, name, ..
+        mint,
+        symbol,
+        name,
+        logo_uri,
+        ..
     }: &SanctumLst,
     ignore_symbols: &HashSet<&str>,
 ) -> Result<Option<()>, Box<dyn Error>> {
@@ -63,7 +77,8 @@ fn verify_lst_token_metadata(
     }
     let (metadata_pk, _bump) = Metadata::find_pda(mint);
     let metadata_acc = match rpc
-        .get_account_with_commitment(&metadata_pk, CommitmentConfig::processed())?
+        .get_account_with_commitment(&metadata_pk, CommitmentConfig::processed())
+        .await?
         .value
     {
         Some(a) => a,
@@ -80,6 +95,24 @@ fn verify_lst_token_metadata(
                 "Expected {field_name} {expected_str} but onchain is {onchain_str}"
             )
             .into());
+        }
+    }
+
+    let uri = metadata.uri.replace('\0', "");
+    if !uri.is_empty() {
+        let OffchainMetadata { image } = client
+            .get(uri)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await?;
+        // use starts_with to account for file extensions
+        // e.g. https://arweave.net/ABC vs https://arweave.net/ABC.svg
+        if !image.starts_with(logo_uri) && !logo_uri.starts_with(&image) {
+            return Err(
+                format!("Expected logo URI {logo_uri} but onchain-linked is {image}").into(),
+            );
         }
     }
 
